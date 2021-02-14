6a2e2c5994bf7de8fe41c7cd11c9af4acdae0516
bellaroseee <brose.workspace@gmail.com>
2021-02-13 18:54:48 -0800

    Update myprogram.py
    implemented save, load, run_pred, load_training & __init__

--- src/myprogram.py
+++ src/myprogram.py
@@ -2,7 +2,15 @@
 import os
 import string
 import random
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import numpy as np
+import random
+import io
 import pandas as pd
+
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
@@ -10,22 +18,56 @@ class MyModel:
     """
     This is a starter model to get you started. Feel free to modify this file.
     """
+    data = []
+    chars = list()
+    char_indices = dict()
+    indices_char = dict()
+    maxlen = 40
+    step = 3
+    model = keras.Sequential()
 
+    def __init__(self):
+        # Load Data
+        path_to_file = keras.utils.get_file(
+        "Processed_Atels", 
+        "https://raw.githubusercontent.com/bellaroseee/447-Group-Project/checkpoint-2/src/Processed_Atels.csv")
+        data = pd.read_csv(path_to_file)
+        MyModel.data = data["Text processed"]
+
+        # create dictionary
+        text = ""
+        for row in MyModel.data:
+            text += row
+        MyModel.chars = sorted(list(set(text)))
+        MyModel.char_indices = dict((c, i) for i, c in enumerate(MyModel.chars))
+        MyModel.indices_char = dict((i, c) for i, c in enumerate(MyModel.chars))
+
+        # model
+        model = keras.Sequential( # stack layers into tf.keras.Model.
+            [ # this is the first layer
+            
+                keras.Input(shape=(MyModel.maxlen, len(MyModel.chars))), # instatntiate Keras tensor of shape (40, 180)
+                layers.LSTM(128), # 128 is the dimensionality of output space
+                layers.Dense(len(MyModel.chars), activation="softmax"), # densely connected NN layer with output of dimension 40 & softmax activation function.
+            ], 
+        )
+        optimizer = keras.optimizers.RMSprop(learning_rate=0.01)
+        model.compile(loss="categorical_crossentropy", optimizer=optimizer)
+        MyModel.model = model
+    
     @classmethod
     def load_training_data(cls):
-        # your code here
-        data = pd.read_csv('Processed_Atels.csv')
-        text_processed = data["Text processed"]
+        # data, chars, char_indices, indices_char = MyModel.prep()
+        chars = MyModel.chars
+        char_indices = MyModel.char_indices
+        indices_char = MyModel.indices_char
+        train_data = MyModel.data[20:]
         text = ""
-        for row in text_processed:
+        for row in train_data:
             text += row
 
-        chars = sorted(list(set(text)))
-        char_indices = dict((c, i) for i, c in enumerate(chars))
-        indices_char = dict((i, c) for i, c in enumerate(chars))
-
-        maxlen = 40
-        step = 3
+        maxlen = MyModel.maxlen
+        step = MyModel.step
         sentences = []
         next_chars = []
         for i in range(0, len(text) - maxlen, step):
@@ -49,6 +91,8 @@ class MyModel:
             for line in f:
                 inp = line[:-1]  # the last character is a newline
                 data.append(inp)
+        # this is for creating test data from data source
+        # test_data = MyModel.data[:10]
         return data
 
     @classmethod
@@ -60,32 +104,53 @@ class MyModel:
     def run_train(self, data, work_dir):
         # your code here
         x, y = data
-        pass
+        batch_size = 128 #HYPERPARAMETER
+        MyModel.model.fit(x, y, batch_size=batch_size, epochs=1)
+        print("Finish training")
 
     def run_pred(self, data):
         # your code here
-        preds = []
+        prediction = []
+
+        model = MyModel.model
+        maxlen = MyModel.maxlen
+        chars = MyModel.chars
+        char_indices = MyModel.char_indices
+        indices_char = MyModel.indices_char
+
         all_chars = string.ascii_letters
         for inp in data:
-            # this model just predicts a random character each time
-            top_guesses = [random.choice(all_chars) for _ in range(3)]
-            preds.append(''.join(top_guesses))
-        return preds
+            guess = ""
+            for i in range(3):
+                x_pred = np.zeros((1, maxlen, len(chars)))
+                for t, char in enumerate(inp): 
+                    # print(f"{t}, {char}")
+                    x_pred[0, t, char_indices[char]] = 1.0
+                preds = model.predict(x_pred, verbose=0)[0]
+                next_index = self.sample(preds, 0.5) # diversity 0.5
+                next_char = indices_char[next_index]
+                guess += next_char
+            print(f"{guess}\n")
+            prediction.append(''.join(guess))
+        return prediction
+    
+    def sample(self, preds, temperature=1.0):
+        # helper function to sample an index from a probability array
+        preds = np.asarray(preds).astype("float64")
+        preds = np.log(preds) / temperature # why do this?
+        exp_preds = np.exp(preds)
+        preds = exp_preds / np.sum(exp_preds) # why do this? normalize?
+        probas = np.random.multinomial(1, preds, 1)
+        return np.argmax(probas)
 
     def save(self, work_dir):
-        # your code here
-        # this particular model has nothing to save, but for demonstration purposes we will save a blank file
-        # with open(os.path.join(work_dir, 'model.checkpoint'), 'wt') as f:
-        #     f.write('dummy save')
-        x = 1
+        model = MyModel.model
+        model.save(work_dir)
 
     @classmethod
     def load(cls, work_dir):
-        # your code here
-        # this particular model has nothing to load, but for demonstration purposes we will load a blank file
-        # with open(os.path.join(work_dir, 'model.checkpoint')) as f:
-        #    dummy_save = f.read()
-        # return MyModel()
+        model = keras.models.load_model(work_dir)
+        MyModel.model = model
         return MyModel()
 
 
@@ -93,7 +158,7 @@ if __name__ == '__main__':
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('mode', choices=('train', 'test'), help='what to run')
     parser.add_argument('--work_dir', help='where to save', default='work')
-    parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
+    parser.add_argument('--test_data', help='path to test data', default='../example/input.txt')
     parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
     args = parser.parse_args()
 
