9927e073b46f04db7e00c61a8059f7fcbdc079be
manginoja <36826929+manginoja@users.noreply.github.com>
2021-02-26 20:27:45 -0800

    Update predict.sh

--- src/predict.sh
+++ src/predict.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 set -v
-python src/languagePrediction.py > language.txt
-python src/myprogram.py test --work_dir $(cat language.txt) --test_data $1 --test_output $2
+python src/main.py --test_data $1 --test_output $2
