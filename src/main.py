00829519186a2e2e48e0ec7b4661c2afbe641489
Gabrielle Lawrencia <glawrenc@nlpg02.cs.washington.edu>
2021-03-12 18:01:53 -0800

    main cleanup and ru work cleanup

--- src/main.py
+++ src/main.py
@@ -16,48 +16,41 @@ from os import listdir
 
 if __name__=="__main__":
   parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-  parser.add_argument('--test_data', help='path to test data', default='test_data/ru-input.txt')
+  parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
   parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
   args = parser.parse_args()
 
-  # f = open(args.test_data, "r")
+  # ld = listdir("test_data")
 
-  # ld = LanguageDetection()
+  # for fn in ld:
+  #   if ("input" not in fn):
+  #     continue
+  #   f = open("test_data/" + fn, "r")
 
-  # langlistperline = []
+  #   lang = detect(f.read())
 
-  # for line in f:
-  #   line = line[-1]
-  #   lang = ld.predictLanguage(line)
-  #   langlistperline.append(lang)
+  #   f.close()
 
-  ld = listdir("test_data")
+  f = open(args.test_data, "r")
+  lang = detect(f.read())
+  f.close()
 
-  for fn in ld:
-    if ("input" not in fn):
-      continue
-    f = open("test_data/" + fn, "r")
+  set_of_covered_languages = set({"en", "ru", "ja", "ch", "it", "de", "fr", "es", "hi"})
 
-    lang = detect(f.read())
+  if lang == "zh-cn":
+    lang = "ch"
+  if lang not in set_of_covered_languages:
+    lang = "en"
 
-    f.close()
-
-    set_of_covered_languages = set({"en", "ru", "ja", "ch", "it", "de", "fr", "es", "hi"})
-
-    if lang == "zh-cn":
-      lang = "ch"
-    if lang not in set_of_covered_languages:
-      lang = "en"
-
-    print('Loading model')
-    workdir = "work/" + lang + "_work"
-    model = MyModel.load(lang, workdir)
-    print('Loading test data from {}'.format(args.test_data))
-    test_data = MyModel.load_test_data(args.test_data)
-    print('Making predictions')
-    pred = model.run_pred(test_data)
-    print('Writing predictions to {}'.format(args.test_output))
-    assert len(pred) == len(test_data), 'Expected {} predictions but got {}'.format(len(test_data), len(pred))
-    model.write_pred(pred, args.test_output)
+  print('Loading model')
+  workdir = "work/" + lang + "_work"
+  model = MyModel.load(lang, workdir)
+  print('Loading test data from {}'.format(args.test_data))
+  test_data = MyModel.load_test_data(args.test_data)
+  print('Making predictions')
+  pred = model.run_pred(test_data)
+  print('Writing predictions to {}'.format(args.test_output))
+  assert len(pred) == len(test_data), 'Expected {} predictions but got {}'.format(len(test_data), len(pred))
+  model.write_pred(pred, args.test_output)
 
 
