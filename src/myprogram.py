3fa9da43aea6eee324586e342347401d753cf555
manginoja <36826929+manginoja@users.noreply.github.com>
2021-03-12 16:53:51 -0800

    Update myprogram.py

--- src/myprogram.py
+++ src/myprogram.py
@@ -123,12 +123,6 @@ class MyModel:
             data = pd.read_csv(path_to_file, nrows=1000)
             print(data.head())
             MyModel.data = data["Text processed"]
-        elif (lang == "ru"):
-             # Load data
-            path_to_file = keras.utils.get_file(fname, url)
-            data = pd.read_csv(path_to_file, nrows=3000)
-            print(data.head())
-            MyModel.data = data["Text processed"]
         else:
             # Load data
             path_to_file = keras.utils.get_file(fname, url)
